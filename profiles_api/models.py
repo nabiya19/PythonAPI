1deaa0fe018a854ad07b423e9da3a52ffcf3baef
nabiya <alamnabiya@outlook.com>
2022-06-03 20:08:17 -0700

    added custom user profile model, manager

---
+++ profiles_api/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
+class UserProfileManager():
+    """manager for user profile"""
+    def create_user(self, email, name, Password=None):
+        if not email:
+            raise ValueError('Users must have email address')
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+        user.setPassword(password)
+        user.save(using=self._db)
+        return user
+    def create_superuser(self, email, name, password):
+        user=self.create_user(email, name, password)
+        user.is_superuser = True
+        uder.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database models for users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """ retrieve full name of user"""
+        return self.name
+
+    def get_short_name(self):
+        """ retrieve short name of user"""
+        return self.name
+
+    def __str__(self):
+        """return string representation of user"""
+        return self.email
