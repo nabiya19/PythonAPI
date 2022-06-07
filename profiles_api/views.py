322e48cfc2d2d7936b1ea4a52d764afa8aea86b1
nabiya <alamnabiya@outlook.com>
2022-06-07 16:48:45 -0700

    profile feed api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,15 +1,15 @@
-from rest_framework import status
-from rest_framework import filters
-from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.authentication import TokenAuthentication
 
-from profiles_api import models
 from profiles_api import serializers
+from profiles_api import models
 from profiles_api import permissions
 
 
