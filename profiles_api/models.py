447b732436994eacafe2594f8bc3374999d033a1
nabiya <alamnabiya@outlook.com>
2022-06-06 20:49:06 -0700

    added authtentication

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,20 +2,19 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
+from django.conf import settings
 
-# Create your models here.
 
 class UserProfileManager(BaseUserManager):
-    """Helps Django work with our custom user model."""
+    """Manager for user profiles"""
 
     def create_user(self, email, name, password=None):
-        """Creates a new user profile object."""
-
+        """Create a new user profile"""
         if not email:
-            raise ValueError('Users must have an email address.')
+            raise ValueError('User must have an email address')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
+        user = self.model(email=email, us=name)
 
         user.set_password(password)
         user.save(using=self._db)
@@ -23,24 +22,21 @@ class UserProfileManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, name, password):
-        """Creates and saves a new superuser with given details."""
-
+        """Create and save a new superuser with given details"""
         user = self.create_user(email, name, password)
 
         user.is_superuser = True
         user.is_staff = True
-
         user.save(using=self._db)
 
         return user
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
-    """Respents a "user profile" inside our system."""
-
+    """Database model for users in the system"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    is_active = models .BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserProfileManager()
@@ -49,29 +45,27 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
-        """Used to get a users full name."""
-
+        """Retrieve full name of user"""
         return self.name
 
     def get_short_name(self):
-        """Used to get a users short name."""
-
+        """Retrieve shot name of user"""
         return self.name
 
     def __str__(self):
-        """Django uses this when it needs to convert the object to a string"""
-
+        """Return string representation of our user"""
         return self.email
 
 
 class ProfileFeedItem(models.Model):
-    """Profile status update."""
-
-    user_profile = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+    """Profile status update"""
+    user_profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
     status_text = models.CharField(max_length=255)
     created_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        """Return the model as a string."""
-
+        """Return the model as a string"""
         return self.status_text
